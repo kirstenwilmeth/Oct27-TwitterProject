6c8ad71592ebeaf1cb906a893bf1d2036ab4b72c
Kirsten <>
2020-10-27 20:52:02 -0500

    re commit

---
+++ src/main/java/com/tts/twitter/repository/TweetRepository.java
@@ -0,0 +1,20 @@
+// package com.tts.twitter.repository;
+
+// import org.springframework.data.repository.CrudRepository;
+// import org.springframework.stereotype.Repository;
+// import com.tts.twitter.model.Tweet;
+
+// import java.util.List;
+
+// import com.tts.twitter.model.User;
+
+
+// @Repository
+// public interface TweetRepository extends CrudRepository<Tweet, Long>{
+// List<Tweet> findAllByOrderByCreatedAtDesc();
+// List<Tweet> findAllByUserOrderByCreatedAtDesc(User user);
+// List<Tweet> findAllByUserInOrderByCreatedAtDesc(List<User> users);
+
+// }
+    
+
